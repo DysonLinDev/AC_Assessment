ac468af2beb16c715779bc6742dd002c72038d4a
Dyson <linshihting1029@gmail.com>
2016-05-18 08:47:15 +0800

    Add Q1 Q2 Q3

---
+++ Q3/Q3/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Q3
+//
+//  Created by MBPrDyson on 5/18/16.
+//  Copyright © 2016 MBPrDyson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("\(#function)")
+    }
+    override func viewDidLayoutSubviews() {
+        print("\(#function)")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        print("\(#function)")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
