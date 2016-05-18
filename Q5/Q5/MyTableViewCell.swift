194ea37aee14e504f27f6c5af74f691839844f85
Dyson <linshihting1029@gmail.com>
2016-05-18 09:33:42 +0800

    Add Q5

---
+++ Q5/Q5/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  Q5
+//
+//  Created by MBPrDyson on 5/18/16.
+//  Copyright © 2016 MBPrDyson. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
