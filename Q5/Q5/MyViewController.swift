c5489ca864540f22f803cabe5198e910feef80c4
Dyson <linshihting1029@gmail.com>
2016-05-18 09:49:49 +0800

    set layout

---
+++ Q5/Q5/MyViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MyViewController.swift
+//  Q5
+//
+//  Created by MBPrDyson on 5/18/16.
+//  Copyright © 2016 MBPrDyson. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController, UITableViewDelegate , UITableViewDataSource {
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.myTableView.delegate = self
+        self.myTableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func takePhotoAction(sender: AnyObject) {
+    }
+
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyTableViewCell") as! MyTableViewCell
+        
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
