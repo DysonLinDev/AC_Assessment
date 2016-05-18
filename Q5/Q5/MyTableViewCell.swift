926d75d7344f63cae462b006779f179d1a0544b7
Dyson <linshihting1029@gmail.com>
2016-05-18 09:36:19 +0800

    Add image and label into cell

--- Q5/Q5/MyTableViewCell.swift
+++ Q5/Q5/MyTableViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MyTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var contentLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
